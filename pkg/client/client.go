62562603458350433d200ae0b9e3d6e03639d22e
Rinaldo Pitzer Jr <16694899+rinaldodev@users.noreply.github.com>
2023-09-17 17:29:10 -0300

    improvement: encapsulate client for testing

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 
 	camelv1 "github.com/apache/camel-k/pkg/apis/camel/v1"
+	camelv1alpha "github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
 	camelclient "github.com/apache/camel-k/pkg/client"
 	"github.com/go-logr/logr/slogr"
 	controllerlog "sigs.k8s.io/controller-runtime/pkg/log"
@@ -13,11 +14,17 @@ import (
 
 var logger = slog.Default().With(slog.String("component", "k8s-client"))
 
-type Client struct {
-	camelclient.Client
+type ScoClient interface {
+	ListPipes(c context.Context) (*camelv1alpha.KameletBindingList, error)
 }
 
-func NewClient() (*Client, error) {
+type defaultScoClient struct {
+	camelCl camelclient.Client
+}
+
+var _ ScoClient = &defaultScoClient{}
+
+func NewClient(c context.Context) (ScoClient, error) {
 	controllerlog.SetLogger(slogr.NewLogr(logger.Handler()))
 
 	cl, err := camelclient.NewClient(false)
@@ -26,7 +33,7 @@ func NewClient() (*Client, error) {
 	}
 
 	ip := &camelv1.IntegrationPlatformList{}
-	err = cl.List(context.Background(), ip)
+	err = cl.List(c, ip)
 	if err != nil {
 		logger.Warn("Failed to find IntegrationPlatform.", err)
 	}
@@ -34,5 +41,14 @@ func NewClient() (*Client, error) {
 		logger.Warn("Failed to find IntegrationPlatform. Is Camel K running?")
 	}
 
-	return &Client{cl}, nil
+	return &defaultScoClient{cl}, nil
+}
+
+func (cl *defaultScoClient) ListPipes(c context.Context) (*camelv1alpha.KameletBindingList, error) {
+	list := &camelv1alpha.KameletBindingList{}
+	err := cl.camelCl.List(c, list)
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
 }
---
+++ test/client/testclient.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"context"
+
+	camelv1alpha "github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
+	"github.com/sco1237896/sco-backend/pkg/client"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type TestClient struct {
+}
+
+var _ client.ScoClient = &TestClient{}
+
+func (cl TestClient) ListPipes(_ context.Context) (*camelv1alpha.KameletBindingList, error) {
+	list := &camelv1alpha.KameletBindingList{
+		Items: []camelv1alpha.KameletBinding{
+			{
+				ObjectMeta: v1.ObjectMeta{
+					Name: "mykb1",
+				},
+			},
+			{
+				ObjectMeta: v1.ObjectMeta{
+					Name: "mykb2",
+				},
+			},
+		},
+	}
+	return list, nil
+}
