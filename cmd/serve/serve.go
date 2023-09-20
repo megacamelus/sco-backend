2e542b2ff46e93f14f6a57e278483b0f8df12184
Rinaldo Pitzer Jr <16694899+rinaldodev@users.noreply.github.com>
2023-09-20 16:09:45 -0300

    refactor: standardize

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -24,15 +24,11 @@ func NewServeCmd() *cobra.Command {
 		Development: false,
 	}
 
-	options := server.Options{
+	serverOpts := server.Options{
 		Addr: ":8080",
 	}
 
-	ho := struct {
-		Health        bool
-		HealthAddress string
-		HealthPrefix  string
-	}{
+	healthOpts := health.Options{
 		Health:        true,
 		HealthPrefix:  health.DefaultPrefix,
 		HealthAddress: health.DefaultAddress,
@@ -46,35 +42,44 @@ func NewServeCmd() *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			done := make(chan os.Signal, 1)
-			signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+			logger.L.Debug("Initializing shutdown channel")
+			shutdown := make(chan os.Signal, 1)
+			signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 
+			logger.L.Info("Initializing Health Check server")
 			var h *health.Service
-
-			if ho.Health {
-				h = health.New(ho.HealthAddress, ho.HealthPrefix, logger.L)
-
-				if err := h.Start(cmd.Context()); err != nil {
-					return err
-				}
+			if healthOpts.Health {
+				h := health.New(healthOpts.HealthAddress, healthOpts.HealthPrefix, logger.L)
+				go func() {
+					if err := h.Start(cmd.Context()); err != nil {
+						logger.L.ErrorContext(cmd.Context(), "Error in Health Check service", slog.Any("error", err))
+						shutdown <- syscall.SIGTERM
+					}
+				}()
 			}
 
+			logger.L.Info("Initializing SCO client")
 			cl, err := client.New(cmd.Context())
 			if err != nil {
 				return err
 			}
 
+			logger.L.Info("Initializing main server")
 			go func() {
-				if err := server.Start(options, cl); err != nil {
-					panic(err)
+				if err := server.Start(serverOpts, cl); err != nil {
+					logger.L.ErrorContext(cmd.Context(), "Error starting main server", slog.Any("error", err))
+					shutdown <- syscall.SIGTERM
 				}
 			}()
 
-			<-done
+			logger.L.Info("Main thread running until shutdown signal")
+			<-shutdown
+			logger.L.Info("Main thread is shutting down")
 
+			logger.L.Info("Terminating Health Check server")
 			if h != nil {
 				if err := h.Stop(cmd.Context()); err != nil {
-					logger.L.Error("error stopping the health service", slog.Any("error", err))
+					logger.L.ErrorContext(cmd.Context(), "Error stopping the health service", slog.Any("error", err))
 				}
 			}
 
@@ -82,11 +87,11 @@ func NewServeCmd() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVar(&options.Addr, "bind-address", options.Addr, "The address the server binds to.")
+	cmd.Flags().StringVar(&serverOpts.Addr, "bind-address", serverOpts.Addr, "The address the server binds to.")
 	cmd.Flags().BoolVar(&logOpts.Development, "dev", logOpts.Development, "Turn on/off development mode")
-	cmd.Flags().BoolVar(&ho.Health, "health-check-enabled", ho.Health, "health-check-enabled")
-	cmd.Flags().StringVar(&ho.HealthPrefix, "health-check-prefix", ho.HealthPrefix, "health-check-prefix")
-	cmd.Flags().StringVar(&ho.HealthAddress, "health-check-address", ho.HealthAddress, "health-check-address")
+	cmd.Flags().BoolVar(&healthOpts.Health, "health-check-enabled", healthOpts.Health, "health-check-enabled")
+	cmd.Flags().StringVar(&healthOpts.HealthPrefix, "health-check-prefix", healthOpts.HealthPrefix, "health-check-prefix")
+	cmd.Flags().StringVar(&healthOpts.HealthAddress, "health-check-address", healthOpts.HealthAddress, "health-check-address")
 
 	return &cmd
 }
